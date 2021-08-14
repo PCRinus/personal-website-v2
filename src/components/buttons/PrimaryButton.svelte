983d09151e2a2456771a94b9e80231d98397aed1
Mircea Casapu <mircea.casapu@gmail.com>
2021-08-14 17:08:58 +0300

    added event for primary buttons

--- src/components/buttons/PrimaryButton.svelte
+++ src/components/buttons/PrimaryButton.svelte
@@ -1,9 +1,17 @@
 <script>
+	import { createEventDispatcher } from 'svelte';
 	export let disabled = '';
+
+	const dispatch = createEventDispatcher();
+
+	function click() {
+		dispatch('click');
+	}
 </script>
 
 <button
 	class="w-32 h-8 lg:w-48 lg:h-12 my-4 text-gray-100 bg-purple-600 hover:bg-purple-700 hover:text-gray-100 rounded-sm disabled:opacity-50 {disabled}"
+	on:click={click}
 >
 	<slot />
 </button>
