b2540ce2ec8dd6f70c1fbe87eac441189e7d40bd
Mircea Casapu <mircea.casapu@gmail.com>
2021-08-15 20:24:01 +0300

    new error message graphic

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,1 +1,10 @@
-<h1 class="text-5xl subpixel-antialiased text-pink-600 dark:text-green-500">Error</h1>
+<script>
+	import ServerDown from '../images/ServerDown.svelte';
+	import { language, theme } from '../stores';
+    import translations from '../translations';
+</script>
+
+<div class="w-full">
+    <ServerDown themeChange={$theme} primary={$theme === 'light' ? '#BE185D' : '#10B981'} />
+	<h1 class="text-3xl mt-8 md:text-5xl subpixel-antialiased text-pink-600 dark:text-green-500">{translations.pages.error.error_message[$language]}</h1>
+</div>
