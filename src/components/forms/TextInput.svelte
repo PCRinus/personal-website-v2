2ae9e6695140a5b65c9131586a2a1188f828903d
Mircea Casapu <mircea.casapu@gmail.com>
2021-08-17 17:44:49 +0300

    small visual bug with darkmode and form inputs

--- src/components/forms/TextInput.svelte
+++ src/components/forms/TextInput.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <input
-	class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+	class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
 	type="text"
 	{name}
 />
