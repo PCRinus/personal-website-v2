b4671802c2c64534bbde4d98c4e2aab274ff494d
Mircea Casapu <mircea.casapu@gmail.com>
2021-08-19 12:28:26 +0300

    improved form succes page

--- src/routes/form_success.svelte
+++ src/routes/form_success.svelte
@@ -1,6 +1,30 @@
 <script>
+	import PrimaryButton from '../components/buttons/PrimaryButton.svelte';
 	import FormSubmitted from '../images/FormSubmitted.svelte';
+	import { language, theme } from '../stores';
+	import translations from '../translations';
+
+	const succesMessage = translations.pages.form_success;
 </script>
 
-<FormSubmitted />
-<h1>Form submitted!!</h1>
+<div class="flex flex-col md:flex-row">
+	<div class="mx-auto md:mx-12 my-4 w-48 md:w-1/4">
+		<FormSubmitted
+			themeChange={$theme}
+			checkColor={$theme === 'light' ? '#BE185D' : '#10B981'}
+			phoneBackgroundColor={$theme === 'light' ? '#BE185D' : '#10B981'}
+		/>
+	</div>
+	<div class="">
+		<h1 class="mb-4 text-3xl lg:text-5xl subpixel-antialiased text-pink-600 dark:text-green-500">
+			{succesMessage.header[$language]}
+		</h1>
+		<h1 class="mb-4 text-xl lg:text-3xl subpixel-antialiased text-pink-600 dark:text-green-500">
+			{succesMessage.thanks[$language]}
+		</h1>
+		<h1>{succesMessage.subheader[$language]}</h1>
+		<PrimaryButton bg_color={'bg-linkedin'}
+			><a href="https://www.linkedin.com/in/mircea-casapu/">{succesMessage.reach_out[$language]}</a>
+		</PrimaryButton>
+	</div>
+</div>
