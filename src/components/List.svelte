1f8c38e0bd971cd56579775b5fce53eb2dd120a7
PCRinus <mircea.casapu@gmail.com>
2021-11-08 01:01:18 +0200

    adjusted margins

--- src/components/List.svelte
+++ src/components/List.svelte
@@ -5,11 +5,11 @@
 
 <div class="mt-2">
 	<h2
-		class="lg:text-2xl my-2 uppercase font-semibold subpixel-antialiased text-pink-600 dark:text-green-500"
+		class="lg:text-2xl uppercase font-semibold subpixel-antialiased text-pink-600 dark:text-green-500"
 	>
 		{listHeader}
 	</h2>
-	<ul class="p-4">
+	<ul class="px-4 py-2">
 		{#each listItems as item}
 			<li>{item}</li>
 		{/each}
