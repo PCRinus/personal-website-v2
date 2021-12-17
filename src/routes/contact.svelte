a35738096fd8868b68efd2774433ecd515b1ca94
PCRinus <mircea.casapu@gmail.com>
2021-12-17 20:59:06 +0200

    more forms refactoring

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -12,46 +12,40 @@
 	{form.form_header[$language]}
 </h1>
 
-<form name="contact" action="form_success" method="post" netlify netlify-honeypot="bot-field">
+<form name="contact" method="post" netlify netlify-honeypot="bot-field">
 	<div class="flex flex-col my-4 max-w-lg">
 		<input type="hidden" name="form-name" value="contact" />
 		<input class="hidden" type="text" name="bot-field" />
-		<p>
-			<label
-				>{form.form_label_name[$language]}
-				<input
-					type="text"
-					name="name"
-					class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
-				/>
-			</label>
-		</p>
-		<p>
-			<label
-				>{form.form_label_email}
-				<input
-					type="email"
-					name="email"
-					class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
-				/>
-			</label>
-		</p>
-		<p>
-			<label
-				>{form.form_label_message[$language]}
-				<textarea
-					name="message"
-					class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
-				/>
-			</label>
-		</p>
-		<p>
+		<label
+			>{form.form_label_name[$language]}
+			<input
+				type="text"
+				name="name"
+				class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
+			/>
+		</label>
+		<label
+			>{form.form_label_email}
+			<input
+				type="email"
+				name="email"
+				class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
+			/>
+		</label>
+		<label
+			>{form.form_label_message[$language]}
+			<textarea
+				name="message"
+				class="mt-1 lg:mt-3 block w-full rounded-sm border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 bg-white dark:bg-jet-light"
+			/>
+		</label>
+		<div>
 			<PrimaryButton>
 				<span slot="icon">✉️</span>
 				<span slot="text">
 					{form.form_submit_button[$language]}
 				</span>
 			</PrimaryButton>
-		</p>
+		</div>
 	</div>
 </form>
